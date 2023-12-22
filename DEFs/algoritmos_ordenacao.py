0a82d638ef6317ea04fc7084e6b6054f8a51994e
Kesia Pereira <kesiapereira2003@gmail.com>
2023-12-22 14:58:48 -0300

    fix: Mudanca nos metodos de ordenacao.

--- DEFs/algoritmos_ordenacao.py
+++ DEFs/algoritmos_ordenacao.py
@@ -3,8 +3,8 @@ class AlgoritmosOrdenacao:
     def bubble_sort(lista):
         tamanho = len(lista)
 
-        for i in range(tamanho - 1):
-            for j in range(tamanho - i - 1):
+        for i in range(tamanho):
+            for j in range(0, tamanho - i - 1):
                 if lista[j] > lista[j + 1]:
                     lista[j], lista[j + 1] = lista[j + 1], lista[j]
 
@@ -14,15 +14,15 @@ class AlgoritmosOrdenacao:
     def selection_sort(lista):
         tamanho = len(lista)
 
-        for i in range(tamanho - 1):
+        for i in range(tamanho):
             indice_menor = i
 
             for j in range(i + 1, tamanho):
                 if lista[j] < lista[indice_menor]:
                     indice_menor = j
                     
-            if indice_menor != i:
-                lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
+            #if indice_menor != i:
+            lista[i], lista[indice_menor] = lista[indice_menor], lista[i]
 
         return lista
 
@@ -48,54 +48,75 @@ class AlgoritmosOrdenacao:
 
         if tamanho <= 1:
             return lista
+        else:
+            meio = tamanho // 2
 
-        meio = tamanho // 2
+            esquerda = lista[:meio]
+            direita = lista[meio:]
 
-        esquerda = lista[:meio]
-        direita = lista[meio:]
+            AlgoritmosOrdenacao.merge_sort(esquerda)
+            AlgoritmosOrdenacao.merge_sort(direita)
 
-        return AlgoritmosOrdenacao.merge(AlgoritmosOrdenacao.merge_sort(esquerda), AlgoritmosOrdenacao.merge_sort(direita))
+            indice_esquerda = indice_direita = indice_lista = 0
+            tamanho_esquerda = len(esquerda)
+            tamanho_direita = len(direita)
 
-    @staticmethod
-    def merge(esquerda, direita):
-        lista_ordenada = []
-        tamanho_esquerda = len(esquerda)
-        tamanho_direita = len(direita)
+            while indice_esquerda < tamanho_esquerda and indice_direita < tamanho_direita:
+                if esquerda[indice_esquerda] < direita[indice_direita]:
+                    lista[indice_lista] = esquerda[indice_esquerda]
+                    indice_esquerda +=1
+                else:
+                    lista[indice_lista] = direita[indice_direita]
+                    indice_direita += 1
+                indice_lista += 1
 
-        indice_esquerda = indice_direita = 0
-
-        while indice_esquerda < tamanho_esquerda and indice_direita < tamanho_direita:
-            if esquerda[indice_esquerda] < direita[indice_direita]:
-                lista_ordenada.append(esquerda[indice_esquerda])
+            while indice_esquerda < tamanho_esquerda:
+                lista[indice_lista] = esquerda[indice_esquerda]
                 indice_esquerda += 1
-            else:
-                lista_ordenada.append(direita[indice_direita])
+                indice_lista +=1
+
+            while indice_direita < tamanho_direita:
+                lista[indice_lista] = direita[indice_direita]
                 indice_direita += 1
+                indice_lista += 1
 
-        lista_ordenada.extend(esquerda[indice_esquerda:])
-        lista_ordenada.extend(direita[indice_direita:])
+            return lista
 
-        return lista_ordenada
+
+    #@staticmethod
+    #def merge(esquerda, direita):
+    #    lista_ordenada = []
+    #    tamanho_esquerda = len(esquerda)
+    #    tamanho_direita = len(direita)
+    #    indice_esquerda = indice_direita = 0
+
+    #    while indice_esquerda < tamanho_esquerda and indice_direita < tamanho_direita:
+    #        if esquerda[indice_esquerda] < direita[indice_direita]:
+    #            lista_ordenada.append(esquerda[indice_esquerda])
+    #            indice_esquerda += 1
+    #        else:
+    #            lista_ordenada.append(direita[indice_direita])
+    #            indice_direita += 1
+
+    #    lista_ordenada.extend(esquerda[indice_esquerda:])
+    #    lista_ordenada.extend(direita[indice_direita:])
+
+    #    return lista_ordenada
 
     @staticmethod
     def quick_sort(lista):
         tamanho = len(lista)
         if tamanho <= 1:
             return lista
-        
-        pivo = lista[tamanho - 1]
-        esquerda = []
-        direita = []
-        for i in range (tamanho - 1):
-            if lista[i] < pivo:
-                esquerda.append(lista[i])
-            else:
-                direita.append(lista[i])
-
-        return AlgoritmosOrdenacao.quick_sort(esquerda) + [pivo] + AlgoritmosOrdenacao.quick_sort(direita)
+        else:
+            pivot = lista[0]
+            esquerda = [x for x in lista[1:] if x <= pivot]
+            direita = [x for x in lista[1:] if x > pivot]
+            return AlgoritmosOrdenacao.quick_sort(esquerda) + [pivot] + AlgoritmosOrdenacao.quick_sort(direita)
 
     @staticmethod
     def counting_sort(lista):
+        tamanho = len(lista)
         maior_valor = max(lista)
 
         lista_auxiliar = [0] * (maior_valor + 1)
@@ -103,10 +124,15 @@ class AlgoritmosOrdenacao:
         for elemento in lista:
             lista_auxiliar[elemento] += 1
 
-        lista_ordenada = []
-        for i in range(len(lista_auxiliar)):
-            while lista_auxiliar[i] > 0:
-                lista_ordenada.append(i)
-                lista_auxiliar[i] -= 1
+        for i in range(1, maior_valor + 1):
+           lista_auxiliar[i] += lista_auxiliar[i - 1]
+
+        lista_ordenada  = [0] * tamanho
+
+        i = tamanho - 1
+        while i >= 0:
+            lista_ordenada[lista_auxiliar[lista[i]] - 1] = lista[i]
+            lista_auxiliar[lista[i]] -= 1
+            i -= 1
 
         return lista_ordenada
